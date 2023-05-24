fc6d04c01101093b52ef2a54d2c1d6415168fefb
VirLg <19virlg@gmail.com>
2023-05-24 22:53:30 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react';
 export const App = () => {
+  const [filmList, setFilmList] = useState([]);
+  const BASE_URL = 'https://api.themoviedb.org/3/movie/550?';
+  const API_KEY = 'd0d7894e72847cf4bdccbd92204adc61';
+  const ENDPOINT = 'trending/all/{time_window}';
+
+  const getFilm = () => {
+    return fetch(
+      'https://api.themoviedb.org/3/trending/all/day?api_key=d0d7894e72847cf4bdccbd92204adc61'
+    ).then(resp => resp.json());
+  };
+
+  getFilm().then(data => {
+    // setFilmList(data);
+    console.log(data);
+  });
+  // useEffect(() => {
+  //   console.log(filmList);
+  // }, []);
   return (
     <div
       style={{
@@ -7,7 +26,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
