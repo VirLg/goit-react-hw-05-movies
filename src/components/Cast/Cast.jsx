34dd24d0757e1970a5255238367b020487c59a71
VirLg <19virlg@gmail.com>
2023-06-02 20:38:56 +0300

    add

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import FetchCard from 'components/Api/Api';
 const Cast = () => {
   const [error, setError] = useState('');
+  const [cast, setCast] = useState([]);
   const { movieId } = useParams();
+  const [murkup, setMurkup] = useState([]);
   useEffect(() => {
     FetchCard(`movie/${movieId}/credits`)
       .then(data => data.json())
-      .then(resp => getFormatiogCast(resp))
+      .then(resp => setCast(resp.cast))
       .catch(error => setError(error.message));
   }, [movieId]);
-  console.log(movieId);
-  const getFormatiogCast = () => {};
-  return <div>Cast:{movieId}</div>;
+
+  const render = useMemo(() => {
+    return cast.map(({ profile_path, name, id }) => {
+      return {
+        profile_path: `${profile_path && profile_path.substring(1)}`,
+        name,
+        id,
+      };
+    });
+  }, [cast]);
+
+  return render.map(({ profile_path, name, id }) => {
+    return (
+      <ul key={id}>
+        <li>
+          <img src={profile_path} alt={name} />
+        </li>
+
+        <li>{name}</li>
+      </ul>
+    );
+  });
 };
 
 export default Cast;
+// {murkup && murkup.map( => console.log(name))}
